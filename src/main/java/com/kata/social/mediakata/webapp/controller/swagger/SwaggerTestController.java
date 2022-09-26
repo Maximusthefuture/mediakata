4f3a03f885ef7d674c090181b07d1ef5bf0acb7e
Khaizar1 <khaizar@mail.ru>
2022-09-26 17:20:58 +0300

    убрал лишние проперти и зависимости

--- src/main/java/com/kata/social/mediakata/webapp/controller/swagger/SwaggerTestController.java
+++ src/main/java/com/kata/social/mediakata/webapp/controller/swagger/SwaggerTestController.java
@@ -5,12 +5,11 @@ import com.kata.social.mediakata.model.entity.user.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** C помощью аннотации @Api над классом контроллера можно добавить описание контроллера,
- *  для проверки результата введите в браузере http://localhost:5555/swagger-ui/   */
+ *  для проверки результата введите в браузере http://${DB_HOST}:${DB_PORT}/swagger-ui/   */
 
 @RestController
 @Api(description = "Тестовый контроллер для проверки сваггера")
@@ -18,9 +17,9 @@ public class SwaggerTestController {
 
     /** C помощью аннотации @ApiOperation над методом контроллера можно добавить описание метода*/
 
-    @ApiOperation(value = "Метод users",
-            notes = "Метод users возвращает всех юзеров из БД")
-    @GetMapping("/api/users")
+    @ApiOperation(value = "Метод allUsers",
+            notes = "Метод allUsers возвращает всех юзеров из БД")
+    @GetMapping("/api/swagger/users")
     public ResponseEntity<User> allUsers() {
         return null;
     }
