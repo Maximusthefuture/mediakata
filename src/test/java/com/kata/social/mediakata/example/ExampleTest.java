a5a62af5b9ce339b016c9ef90ccfc9a8509b3dfe
MaximShevtsov <naturemade@yandex.ru>
2022-09-26 11:11:35 +0300

    Убран тестовый метод.

--- src/test/java/com/kata/social/mediakata/example/ExampleTest.java
+++ src/test/java/com/kata/social/mediakata/example/ExampleTest.java
@@ -14,15 +14,5 @@ import java.util.Optional;
 @Sql("/sql/data.sql")
 public class ExampleTest extends AbstractSpringTest {
 
-    @Autowired
-    private UserDao dao;
-
-    @Test
-    void test() {
-        Optional<User> user = dao.getById(1L);
-        String lastName = user.get().getLastName();
-        System.out.println(lastName);
-        Assert.assertEquals(lastName, "petrov");
-    }
 
 }
