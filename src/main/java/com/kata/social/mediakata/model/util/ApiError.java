bd19d3818b825492e78318b5b04367e464e8efa6
Valera2312 <33982431+Valera2312@users.noreply.github.com>
2022-10-06 17:05:25 +1000

    добавлен AdviceController и ApiError

---
+++ src/main/java/com/kata/social/mediakata/model/util/ApiError.java
@@ -0,0 +1,15 @@
+package com.kata.social.mediakata.model.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+
+    private String message;
+    private String debugMessage;
+
+}
